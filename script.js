220b21e9ecd933412d86bbbfb9aa30598cf47cc5
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-29 01:21:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
 //your JS code here. If required.
-// const inputs = document.querySelectorAll('.code');
-
-// inputs.forEach((input, idx) => {
-//     input.addEventListener('keydown', (e) => {
-// 		inputs.forEach(i => i.classList.remove('focused'));
-
-//         input.classList.add('focused');
-//         if (e.key === "Backspace") {
-//             input.value = ''; 
-//             if (idx > 0) {
-//                 inputs[idx - 1].focus(); 
-//             }
-//             e.preventDefault();
-//         } else if (e.key >= '0' && e.key <= '9') {
-//             input.value = ''; 
-//         } else if (e.key !== "Tab") {
-//             e.preventDefault(); 
-//         }
-//     });
-
-//     input.addEventListener('input', () => {
-//         if (input.value.length > 0 && idx < inputs.length - 1) {
-//             inputs[idx + 1].focus(); 
-//         }
-//     });
-// });
-
 const inputs = document.querySelectorAll('.code');
-
-function setFocus(idx) {
-    inputs.forEach(i => i.classList.remove('focused'));
-    inputs[idx].classList.add('focused');
-    inputs[idx].focus();
-}
 
 inputs.forEach((input, idx) => {
     input.addEventListener('keydown', (e) => {
+		inputs.forEach(i => i.classList.remove('focused'));
+
+        input.classList.add('focused');
         if (e.key === "Backspace") {
-            input.value = '';
+            input.value = ''; 
             if (idx > 0) {
-                setFocus(idx - 1); // focus previous input
-            } else {
-                setFocus(0); // stay on first
+                inputs[idx - 1].focus(); 
             }
             e.preventDefault();
         } else if (e.key >= '0' && e.key <= '9') {
@@ -53,10 +21,8 @@ inputs.forEach((input, idx) => {
 
     input.addEventListener('input', () => {
         if (input.value.length > 0 && idx < inputs.length - 1) {
-            setFocus(idx + 1); // focus next input
+            inputs[idx + 1].focus(); 
         }
     });
 });
 
-// Initialize first input with focus
-setFocus(0);
