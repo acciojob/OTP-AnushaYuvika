3d104ba55d93dc4c66dd4f3599a1a621c745e902
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-03-10 17:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((input, index) => {
+	input.addEventListener("input", () => {
+		if(input.value !== "") {
+			const next = input.nextElementSibling;
+
+			if(next) {
+				next.focus();
+			}
+		}
+	})
+
+	input.addEventListener("keydown", (e) => {
+		if(e.key === "Backspace" && input.value === "") {
+			const prev = input.previousElementSibling;
+
+			if(prev) {
+				prev.focus();
+			}
+		}
+	})
+})
+
+codes[0].addEventListener("paste", (e) => {
+
+  const data = e.clipboardData.getData("text").split("");
+
+  codes.forEach((input, index) => {
+    input.value = data[index] || "";
+  });
+
+});
+codes[0].focus();
